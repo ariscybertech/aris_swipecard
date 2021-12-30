f00e930d25c6f4187b3854ebb5210d31a1a0ce58
ariscybertech <ariscybertech@gmail.com>
2021-12-30 21:46:36 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# swipecard
+
+Swipe card example
+
+https://www.youtube.com/watch?v=2yEb9YOwZr8
---
+++ android/app/src/main/kotlin/com/example/aris_swipecard/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aris_swipecard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
